c2c76f666215219395326aaf5125b23c571131d0
Kursulla <bozic85@gmail.com>
2014-11-12 10:19:15 +0100

    Small cleanup

--- app/src/main/java/rs/webnet/floatingactionbutton/MainActivity.java
+++ app/src/main/java/rs/webnet/floatingactionbutton/MainActivity.java
@@ -1,13 +1,20 @@
 package rs.webnet.floatingactionbutton;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 
 public class MainActivity extends Activity {
@@ -21,13 +28,7 @@ public class MainActivity extends Activity {
 
         FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
 
-
-
-
-
-
-
-        fab.setDimmedBackgroundColor("#99000000");
+        fab.setDimmedBackgroundColor("#99FFFFFF");
         fab.setIconOpened(R.drawable.ic_fab_opened);
         fab.setIconClosed(R.drawable.ic_fab_closed);
         fab.setButtonBackground(R.drawable.ic_fab_bgd_opened);
@@ -36,51 +37,24 @@ public class MainActivity extends Activity {
         fab.setAnimationSpeed(300);
 
 
+        _setupFabContent(fab);
+    }
 
 
 
 
-
-
+    private void _setupFabContent(FloatingActionButton fab) {
         for(int i=0;i<5;i++) {
-            Button tv = new Button(this);
-            tv.setPadding(10,10,10,10);
-            tv.setText("Button "+i);
-            fab.addItem(tv);
+            LayoutInflater inflater = getLayoutInflater();
+            View item = inflater.inflate(R.layout.item,null);
+            TextView textView = (TextView) item.findViewById(R.id.name);
+            textView.setText("Call User "+i);
+            fab.addItem(item);
         }
 
         ImageView imageView = new ImageView(this);
         imageView.setImageResource(R.drawable.ic_launcher);
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "klik");
-            }
-        });
-
         fab.addItem(imageView);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
